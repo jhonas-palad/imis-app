cfffd57d66e09b402488598d2cc408d9e5da3a95
Jhonas Palad <jhonasemmanuel@gmail.com>
2023-02-21 21:27:33 +0800

    categorize packages implemented

--- src/components/ServiceDetails.tsx
+++ src/components/ServiceDetails.tsx
@@ -1,5 +1,5 @@
 import { MotiView } from 'moti';
-import {Box,Center, Text, FlatList, ScrollView, Pressable, Image, Icon, Heading, HStack, IconButton} from 'native-base';
+import {Box,Center, Text, FlatList, ScrollView, Pressable, Image, Icon, Heading, HStack, IconButton, Badge} from 'native-base';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
 import { ServiceNavigationProp, ServiceRouteProp } from '../navigation';
@@ -33,10 +33,22 @@ const ServiceDetails: React.FC = () => {
     useEffect(()=> {
         const service = services.filter(value => value.id === route.params.serviceId)[0];
         if(!(service.categories.find((item)=> item.categoryName === 'All'))){
-            service.categories = [{id: makeID(), categoryName: 'All'},...service.categories]
+            service.categories = [{id: 'a0482a93-8790-aeea287f08a2', categoryName: 'All'},...service.categories]
         }
         setServiceDetails((prevDetails)=> ({...service}));
+        setPackageCategories([...service.packages]);
     }, [route]);
+
+    const handleCategoryFilter = React.useCallback((categoryId : string)=> {
+        console.log(categoryId);
+        if(categoryId === 'a0482a93-8790-aeea287f08a2'){
+            setPackageCategories([...serviceDetails.packages])
+        }
+        else{
+            setPackageCategories([...serviceDetails.packages.filter( item => item.categories.includes(categoryId))])
+        }
+    }, [serviceDetails]);
+
 
     return (
         <Box safeAreaTop flex={1} >
@@ -53,7 +65,7 @@ const ServiceDetails: React.FC = () => {
                         renderItem={
                             ({item, index})=>{
                                 return(
-                                    <Pressable key={item.id} marginLeft={index === 0 ? 5: 0} marginRight={5} rounded={12} paddingX={6} paddingY={2} bg="orange.400">
+                                    <Pressable key={item.id} onPress={()=>handleCategoryFilter(item.id)} marginLeft={index === 0 ? 5: 0} marginRight={5} rounded={12} paddingX={6} paddingY={2} bg="orange.400">
                                         <Text fontWeight="600" fontSize="md">{item.categoryName}</Text>
                                     </Pressable>
                                 )
@@ -63,28 +75,30 @@ const ServiceDetails: React.FC = () => {
                 </Box>
                 <Text fontSize="lg" fontWeight="600" ml={3}>Bedroom</Text>
                 {
-                    serviceDetails.packages.map((_package)=>{
+                    packageCategories.map((_package)=>{
                         return (
-                            <Pressable borderBottomColor="coolGray.200" borderBottomWidth={1} rounded={12} padding={3} mx={3}  flexDir="row" key={_package.id} mb={5}>
+                            <Pressable key={_package.id} borderBottomColor="coolGray.200" borderBottomWidth={1} rounded={12} padding={3} mx={3} flexDir="row" mb={5}>
                                 <Center mr={3} paddingBottom={3}>
                                     <Box mb={3} paddingX={3} rounded={12} paddingTop={3} bg="darkBlue.400">
                                         <Image rounded={8} size="xl" alt="women_clean" resizeMode="contain" source={require('../../assets/images/women_bucket.png')} />
                                     </Box>
                                     <HStack alignSelf="stretch" alignItems="center" justifyContent="space-between">
-                                        <IconButton bg="darkBlue.200" icon={<Icon as={Feather} color="coolGray.100" name="minus" size="md"/>} />
+                                        <IconButton bg="darkBlue.200" rounded={12} icon={<Icon as={Feather} color="coolGray.100" name="minus" size="md"/>} />
                                             <Text>0</Text>
-                                        <IconButton bg="darkBlue.200" icon={<Icon as={Feather} name="plus" color="coolGray.100" size="md"/>} />
+                                        <IconButton bg="darkBlue.200" rounded={12} icon={<Icon as={Feather} name="plus" color="coolGray.100" size="md"/>} />
                                     </HStack>
                                 </Center>
                                 
-                                <Box flex={1} bg="amber.300">
-                                    <Text fontSize="md" fontWeight="semibold">{_package.packageName}</Text>
+                                <Box flex={1}>
+                                    <Badge bg="green.600" _text={{color:'white', fontWeight:'600'}} rounded={12} alignSelf="initial">20% discount</Badge>
+                                    <Text fontSize="lg" color="darkBlue.800" fontWeight="bold">{_package.packageName}</Text>
+                                    <Text fontSize="md" fontWeight="semibold">${_package.packageRate}</Text>
                                     <Box flexDir="column" >
                                             {
                                                 _package.packageDescription.map((description)=>{
                                                     return(
-                                                        <Text>
-                                                            - {description}
+                                                        <Text key={makeID()} color="coolGray.500">
+                                                            {description}
                                                         </Text>
                                                     )
                                                 })
